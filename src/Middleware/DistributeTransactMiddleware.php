3276012e405b506c522f8e1a7d176f158f24d463
zhangzibin <619146134@qq.com>
2022-01-30 18:03:42 +0800

    DistributeTransactMiddleware

--- src/Middleware/DistributeTransactMiddleware.php
+++ src/Middleware/DistributeTransactMiddleware.php
@@ -19,6 +19,11 @@ use Psr\Http\Server\MiddlewareInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\RequestHandlerInterface;
 use Hyperf\Utils\Context;
+use Hyperf\Utils\ApplicationContext;
+use Hyperf\Contract\ConfigInterface;
+use Hyperf\HttpServer\Response;
+use Windawake\HyperfResetTransaction\Exception\ResetTransactionException;
+use Windawake\HyperfResetTransaction\Facades\RT;
 
 class DistributeTransactMiddleware implements MiddlewareInterface
 {
@@ -34,37 +39,39 @@ class DistributeTransactMiddleware implements MiddlewareInterface
 
     public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
     {
-        $transactId = $request->getHeaderLine('transact_id');
+        $requestId = $request->getHeaderLine('rt_request_id');
+        $transactId = $request->getHeaderLine('rt_transact_id');
+        $connection = $request->getHeaderLine('rt_connection');
+        if ($connection) {
+            Context::set('rt_connection', $connection);
+        }
+        
         if ($transactId) {
-            $sqlArr = Db::table('reset_transaction')->where('transact_id', $transactId)->pluck('sql')->toArray();
-            $sql = implode(';', $sqlArr);
-            Db::beginTransaction();
-            if ($sqlArr) {
-                Db::unprepared($sql);
+            if (!$requestId) {
+                throw new ResetTransactionException('rt_request_id cannot be null');
+            }
+            Context::set('rt_request_id', $requestId);
+            $item = DB::connection('rt_center')->table('reset_transact_req')->where('request_id', $requestId)->first();
+            if ($item) {
+                $data = json_decode($item->response, true);
+                return Response::json($data);
             }
 
-            Context::set('transact_id', $transactId);
+            RT::middlewareBeginTransaction($transactId, $connection);
         }
 
         $response = $handler->handle($request);
 
-        $transactId = Context::get('transact_id');
-        if ($transactId) {
-            Db::rollBack();
-
-            $sqlArr = Context::get('transact_sql');
-            Context::destroy('transact_id');
-            Context::destroy('transact_sql');
-
-            if ($sqlArr) {
-                foreach ($sqlArr as $item) {
-                    Db::table('reset_transaction')->insert([
-                        'transact_id' => $transactId,
-                        'sql' => value($item['sql']),
-                        'result' => $item['result'],
-                    ]);
-                }
-            }
+        $requestId = $request->getHeaderLine('rt_request_id');
+        $transactId = $request->getHeaderLine('rt_transact_id');
+        $transactIdArr = explode('-', $transactId);
+        if ($transactId && $response->isSuccessful()) {
+            RT::middlewareRollback();
+            DB::connection('rt_center')->table('reset_transact_req')->insert([
+                'transact_id' => $transactIdArr[0],
+                'request_id' => $requestId,
+                'response' => $response->getContent(),
+            ]);
         }
 
         return $response;
